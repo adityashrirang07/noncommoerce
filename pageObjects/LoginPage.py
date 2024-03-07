f7bd93b9bd7e7ffa9256d8c7f58e92633a185566
adityashrirang07 <140724571+adityashrirang07@users.noreply.github.com>
2024-03-07 22:39:05 +0530

    s

--- pageObjects/LoginPage.py
+++ pageObjects/LoginPage.py
@@ -11,9 +11,11 @@ class LoginPage:
     def __init__(self, driver):
         self.driver = driver
 
+
     def setUserName(self, username):
         self.driver.find_element(By.ID, self.textbox_user_id).clear()
         self.driver.find_element(By.ID, self.textbox_user_id).send_keys(username)
+
 
     def setPassword(self, password):
         self.driver.find_element(By.ID, self.textbox_password_id).clear()

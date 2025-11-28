0cdacd7477e44001df826d6f103f88d0d281516a
“adityashrirang” <adishrirang>
2025-11-28 22:06:02 +0530

    Resolved lint issues

--- pageObjects/LoginPage.py
+++ pageObjects/LoginPage.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 
 
-class loginPage:
+class LoginPage:
     textbox_user_id = "Email"
     textbox_password_id = "Password"
     button_login_xpath = "//button[contains(text(),'Log in')]"
@@ -11,16 +11,16 @@ class loginPage:
         self.driver = driver
 
 
-    def setUserName(self, username):
+    def SetUserName(self, username):
         self.driver.find_element(By.ID, self.textbox_user_id).clear()
         self.driver.find_element(By.ID, self.textbox_user_id).send_keys(username)
 
-    def setPassword(self, password):
+    def SetPassword(self, password):
         self.driver.find_element(By.ID, self.textbox_password_id).clear()
         self.driver.find_element(By.ID, self.textbox_password_id).send_keys(password)
 
-    def clickLoginButton(self):
+    def ClickLoginButton(self):
         self.driver.find_element(By.XPATH, self.button_login_xpath).click()
 
-    def clickLogout(self):
+    def ClickLogout(self):
         self.driver.find_element(By.XPATH, self.link_logout_xpath).click()

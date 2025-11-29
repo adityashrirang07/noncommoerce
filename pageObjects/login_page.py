d5d4971f47a279ad347c2c879d8efa1b9722ea69
“adityashrirang” <adishrirang>
2025-11-29 15:10:30 +0530

    Remove unwanted lines

--- pageObjects/login_page.py
+++ pageObjects/login_page.py
@@ -11,19 +11,21 @@ class LoginPage:
         self.driver = driver
 
 
+
+
+
+
+
     def set_username(self, username):
         self.driver.find_element(By.ID, self.textbox_user_id).clear()
         self.driver.find_element(By.ID, self.textbox_user_id).send_keys(username)
-
 
     def set_password(self, password):
         self.driver.find_element(By.ID, self.textbox_password_id).clear()
         self.driver.find_element(By.ID, self.textbox_password_id).send_keys(password)
 
-
     def click_login_button(self):
         self.driver.find_element(By.XPATH, self.button_login_xpath).click()
-
 
     def click_logout(self):
         self.driver.find_element(By.XPATH, self.link_logout_xpath).click()
